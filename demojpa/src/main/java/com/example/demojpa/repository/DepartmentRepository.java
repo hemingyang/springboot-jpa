daf4f5b8a28cbb460222d633f9236db795dc29f2
hemingyang <hemingyangyang@gmail.com>
2020-06-14 13:25:33 +0800

    自定义查询

--- demojpa/src/main/java/com/example/demojpa/repository/DepartmentRepository.java
+++ demojpa/src/main/java/com/example/demojpa/repository/DepartmentRepository.java
@@ -2,9 +2,14 @@ package com.example.demojpa.repository;
 
 import com.example.demojpa.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * 继承jpa
  */
 public interface DepartmentRepository extends JpaRepository<Department,Long> {
+
+
+   // @Query("SELECT  dep FROM  Department  dep where dep.name in (select *from Employee  em where  em.department_id)" )
+    public String  finddepName(String name);
 }
